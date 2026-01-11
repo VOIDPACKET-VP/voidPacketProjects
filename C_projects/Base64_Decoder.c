19eb8fa0cfb5bf4c2f3c793f6515312c65b9f7ac
VOIDPACKET <pincybergwan.224@gmail.com>
2026-01-11 15:52:16 +0100

    Start of the decoder

---
+++ C_projects/Base64_Decoder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';
+    char name[30] = "";
+
+    printf("Enter your age, gpa, grade and name :\n");
+    scanf("%d", &age);
+    scanf("%f", &gpa);
+    scanf(" %c", &grade);
+    getchar();
+    fgets(name, sizeof(name), stdin);
+
+    printf("%d\n", age);
+    printf("%.2f\n", gpa);
+    printf("%c\n", grade);
+    printf("%s\n", name);
+
+    return 0;
+}
