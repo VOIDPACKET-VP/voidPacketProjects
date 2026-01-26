3d2a94b31b779072ba75728b73f0a230712423a4
VOIDPACKET <pincybergwan.224@gmail.com>
2026-01-26 17:50:29 +0100

    Code

--- C_projects/test.c
+++ C_projects/test.c
@@ -117,3 +117,27 @@
 //     printf("%llu", res);
 //     return 0;
 // }
+
+// 263A - Beautiful Matrix
+// #include <stdio.h>
+// #include <math.h>
+// int main(){
+//     int arr[5][5];
+//     int isStopped = 0;
+//     for (int i = 0; i < 5; i++){
+//         for (int j = 0; j < 5; j++){
+//             scanf("%d", &arr[i][j]);
+//             if (arr[i][j] == 1){
+//                 printf("%d", abs(i-3) + abs(j-3));
+//                 isStopped = 1;
+//                 break;
+//             }
+//         }
+//         if (isStopped){
+//             break;
+//         }
+
+//     }
+//     return 0;
+// }
+
