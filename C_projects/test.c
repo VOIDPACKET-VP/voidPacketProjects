db6a77491f237470955ee7cb08c144180f646b29
VOIDPACKET <pincybergwan.224@gmail.com>
2026-01-30 18:35:43 +0100

    code

--- C_projects/test.c
+++ C_projects/test.c
@@ -420,4 +420,37 @@
 //     return 0;
 // }
 
-// 
+// 208A - Dubstep
+// #include <stdio.h>
+// #include <string.h>
+// int main() {
+//     char dub[201];  
+//     scanf("%s", dub);
+    
+//     char nonDub[201];
+//     int nonDub_i = 0;
+//     int len = strlen(dub);
+    
+//     for (int i = 0; i < len; i++) {
+//         if (i + 2 < len && dub[i] == 'W' && dub[i+1] == 'U' && dub[i+2] == 'B') {
+//             i += 2;
+
+//             if (nonDub_i > 0 && nonDub[nonDub_i-1] != ' ') {
+//                 nonDub[nonDub_i] = ' ';
+//                 nonDub_i++;
+//             }
+//         } else {
+//             nonDub[nonDub_i] = dub[i];
+//             nonDub_i++;
+//         }
+//     }
+
+//     nonDub[nonDub_i] = '\0';
+    
+//     if (nonDub_i > 0 && nonDub[nonDub_i-1] == ' ') {
+//         nonDub[nonDub_i-1] = '\0';
+//     }
+    
+//     printf("%s\n", nonDub);
+//     return 0;
+// }
