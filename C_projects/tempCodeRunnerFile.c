30c3250d8380e18ed2b5801c2f04317a24073b30
VOIDPACKET <pincybergwan.224@gmail.com>
2026-01-31 18:17:50 +0100

    code

--- C_projects/tempCodeRunnerFile.c
+++ C_projects/tempCodeRunnerFile.c
@@ -1,1 +1,46 @@
-1
+#include <stdio.h>
+#include <math.h>
+#define max(a, b) (a > b ? a : b)
+int main(){
+    int n;
+    int one = 0, two = 0, three = 0, four = 0;
+    int group, taxi = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        scanf("%d", &group);
+        if (group == 1){
+            one++;
+        } else if (group == 2){
+            two++;
+        } else if (group == 3){
+            three++;
+        } else {
+            four++;
+        }
+    }
+
+    // Groupe of 4
+    taxi += four;
+    four = 0;
+
+    // Groupe of 3
+    taxi += three;
+    one = max(0, one - three);
+    three = 0;
+
+    // Groupe of 2
+    taxi += two/2;
+    two = two % 2;
+
+    // Leftover Group of 2
+    if (two == 1) taxi += 1;
+    one = max(0, one - 2);
+
+    // Leftover Group of 1
+    taxi += ceil((one + 3) / 4);
+
+    printf("%d", taxi);
+
+    return 0;
+}
--- C_projects/test.c
+++ C_projects/test.c
@@ -454,3 +454,93 @@
 //     printf("%s\n", nonDub);
 //     return 0;
 // }
+
+// 25A - Taxi
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(){
+//     int n, even = 0, odd = 0;
+//     int e = 0, o = 0;
+//     int *numbers;
+
+//     scanf("%d", &n);
+//     numbers = (int *)malloc(n * sizeof(int));
+
+//     if (numbers == NULL){
+//         return 1;
+//     }
+
+//     for (int i = 0; i < n; i++){
+//         scanf("%d", &numbers[i]);
+//         if (numbers[i] % 2 == 0){
+//             even++;
+//             e = i + 1;
+//         } else {
+//             odd++;
+//             o = i + 1;
+//         }
+//     }
+
+//     if (odd == 1){
+//         printf("%d", o);
+//     } else {
+//         printf("%d", e);
+//     }
+
+
+//     free(numbers);
+//     numbers = NULL;
+
+//     return 0;
+// }
+
+// 158B - Taxi
+// #include <stdio.h>
+// int main(){
+//     int n;
+//     int one = 0, two = 0, three = 0, four = 0;
+//     int group, taxi = 0;
+//     scanf("%d", &n);
+
+//     for (int i = 0; i < n; i++){
+//         scanf("%d", &group);
+//         if (group == 1){
+//             one++;
+//         } else if (group == 2){
+//             two++;
+//         } else if (group == 3){
+//             three++;
+//         } else {
+//             four++;
+//         }
+//     }
+
+//     // Groupe of 4
+//     taxi += four;
+
+//     // Groupe of 3
+//     taxi += three;
+//     if (one > three) one -= three;
+//     else one = 0;
+
+//     // Groupe of 2
+//     taxi += two / 2;
+//     two = two % 2;
+
+//     // Leftover Group of 2
+//     if (two == 1){
+//         taxi++;
+//         if (one >= 2){
+//             one -= 2;
+//         } else if (one == 1){
+//             one -= 1;
+//         }
+//     }
+
+//     // Leftover Group of 1
+//     taxi += (one + 3) / 4;
+
+//     printf("%d", taxi);
+
+//     return 0;
+// }
